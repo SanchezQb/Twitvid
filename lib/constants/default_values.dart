dd53001d1742f952e856b419d6798a5f8f6e6e79
chisom <chisom.exwuribe@tradebuza.com>
2020-12-05 08:15:22 +0100

    First Release

--- lib/constants/default_values.dart
+++ lib/constants/default_values.dart
@@ -1,4 +1,4 @@
-const kBaseUrl = "https://twitvid.herokuapp.com";
+const kBaseUrl = "https://twitvid.sanch.dev";
 const kAppId = "ca-app-pub-7621785071439759~1954113600";
 const kTestAdUnit = "ca-app-pub-3940256099942544/2247696110";
 const kTestVid = "https://twitter.com/ar_dysn/status/1322968397991333889?s=20";
